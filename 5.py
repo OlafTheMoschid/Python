1ce858576ec524649ad44d34bf357f939b009dd1
OlafTheMoschid <maxseraph721@gmail.com>
2021-09-19 13:34:09 +0700

    Add files via upload

--- 5.py
+++ 5.py
@@ -18,7 +18,8 @@ class Programm(QWidget):
 
         self.meal = [0] * self.lng
         self.price = [0] * self.lng
-        self.btn = [0] * self.lng
+        self.btn1 = [0] * self.lng
+        self.btn2 = [0] * self.lng
         self.cnt = [0] * self.lng
         self.count = [0] * self.lng
 
@@ -32,30 +33,46 @@ class Programm(QWidget):
                 "price : " + str(self.menu.get(self.menul[i])) + " rub", self)
             self.meal[i].move(200, 50 + i * 30)
 
-            self.btn[i] = QPushButton("+", self)
-            self.btn[i].resize(30, 18)
-            self.btn[i].move(300, 48 + i * 30)
-            self.btn[i].clicked.connect(self.buttons)
+            self.btn1[i] = QPushButton("+", self)
+            self.btn1[i].resize(30, 18)
+            self.btn1[i].move(270, 48 + i * 30)
+            self.btn1[i].clicked.connect(self.button1)
+
+            self.btn2[i] = QPushButton("-", self)
+            self.btn2[i].resize(30, 18)
+            self.btn2[i].move(300, 48 + i * 30)
+            self.btn2[i].clicked.connect(self.button2)
 
             self.cnt[i] = QLabel("count : " + str(self.count[i]), self)
             self.cnt[i].move(350, 50 + i * 30)
 
-        self.btn.append(QPushButton("sum", self))
-        self.btn[-1].resize(30, 20)
-        self.btn[-1].move(300, 48 + (self.lng + 1) * 30)
-        self.btn[-1].clicked.connect(self.summ)
+        self.btn2.append(QPushButton("sum", self))
+        self.btn2[-1].resize(30, 20)
+        self.btn2[-1].move(300, 48 + (self.lng + 1) * 30)
+        self.btn2[-1].clicked.connect(self.summ)
 
         self.check = QPlainTextEdit(self)
         self.check.move(450, 50)
         self.check.resize(350, 400)
 
-    def buttons(self):
+    def button1(self):
         send = self.sender()
         for i in range(self.lng):
-            if self.btn[i] == send:
+            if self.btn1[i] == send:
                 self.count[i] += 1
                 self.cnt[i].setText("count : " + str(self.count[i]))
                 self.sum += self.menu.get(self.menul[i])
+
+    def button2(self):
+        send = self.sender()
+        for i in range(self.lng):
+            if self.btn2[i] == send:
+                if self.count[i] < 1:
+                    break
+                else:
+                    self.count[i] -= 1
+                self.cnt[i].setText("count : " + str(self.count[i]))
+                self.sum -= self.menu.get(self.menul[i])
 
     def summ(self):
         for i in range(self.lng):
