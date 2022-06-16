36f232b7780f2596df5010c65ac86daed1bb7693
OlafTheMoschid <maxseraph721@gmail.com>
2022-06-16 22:18:27 +0700

    Delete Semester2 directory

--- Semester2/api_restful_user_add.py
+++
@@ -1,9 +0,0 @@
-from requests import post
-
-user = {
-     'nickname': 'test_name',
-     'email': 'blahblah@goooo.com',
-     'hashed_password': 'password_20'
-}
-
-print(post('http://127.0.0.1:8080/api/v2/users', json=user).json())
--- Semester2/api_restful_user_delete.py
+++
@@ -1,3 +0,0 @@
-from requests import delete
-
-print(delete('http://127.0.0.1:8080/api/v2/users/2').json())
