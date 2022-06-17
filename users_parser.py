dd6c441b8318b44f45aaeb2786c9fc2407d3771f
OlafTheMoschid <maxseraph721@gmail.com>
2022-06-17 18:18:28 +0700

    Delete users_parser.py

--- users_parser.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import reqparse
-
-parser = reqparse.RequestParser()
-
-parser.add_argument('nickname', required=False)
-parser.add_argument('email', required=False)
-parser.add_argument('hashed_password', required=False)
